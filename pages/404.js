2fd260bcc137bcb88a8fc6f39840aa7f23fe7edc
Supawee Borrisutsawas <supawee.b@hotmail.com>
2021-08-03 10:18:39 +0700

    Section 3

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+function NotFountPage () {
+  return (
+    <div>
+      <h1>Page not found</h1>
+    </div>
+  )
+}
+
+export default NotFountPage
---
+++ pages/portfolio/list.js
@@ -0,0 +1,9 @@
+function ListPage () {
+  return (
+    <div>
+      <h1>The Lsit Page</h1>
+    </div>
+  )
+}
+
+export default ListPage
